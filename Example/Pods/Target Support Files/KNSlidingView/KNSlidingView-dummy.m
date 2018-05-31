05000e8ca31a7e5e92dcdc236d636539770d444a
Leessang <992275294@qq.com>
2018-05-31 10:46:07 +0800

    第一次提交

---
+++ Example/Pods/Target Support Files/KNSlidingView/KNSlidingView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KNSlidingView : NSObject
+@end
+@implementation PodsDummy_KNSlidingView
+@end
---
+++ Example/Pods/Target Support Files/KNSlidingView/KNSlidingView-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "KNBillboadrView.h"
+#import "NSTimer+KNTimer.h"
+
+FOUNDATION_EXPORT double KNSlidingViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char KNSlidingViewVersionString[];
+
